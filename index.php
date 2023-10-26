9faddf12456f707cd0cb66724d7512bf697c7d98
Emil <emils.boreiko@gmail.com>
2023-10-26 19:51:33 +0300

    adding images

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Elements Inc</title>
+    <link rel="stylesheet" href="/styles/styles.css">
+
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row-1-header">
+        <h1>Elements Inc.</h1>
+        <h2>Only one Earth</h2>
+    </div>
+    <div class="row-2-col-1-text">
+        <p id="row-2-col-1-text-1">
+            The urgent problem of clean water is inextricably linked with the problem of good wastewater treatment.
+        </p>
+        <p id="row-2-col-1-text-2">
+            Through extensive laboratory tests, pilot trials and development of new principles of operation of deep
+            wastewater treatment technology we achieve effective removal of hazardous elements from water
+        </p>
+    </div>
+    <div class="row-2-col-2-picture"></div>
+    <div class="row-3-text">
+        <h2>
+            The protection of surface waters from pollution is the most important task facing modern society.
+        </h2>
+    </div>
+    <div class="row-4-col-1-text">
+        <h2 id="row-4-col-1-text-1">
+            Hazardous elements
+        </h2>
+        <p id="row-4-col-1-text-2">
+            Lead, chromium, cadmium, benzopyrene, and chlorine in the water cause cancer and nerve problems.
+        </p>
+        <p id="row-4-col-1-text-3">
+            Their effective and comprehensive removal is possible only with the use
+            of advanced wastewater treatment technologies and modern equipment.
+        </p>
+    </div>
+    <div class="row-4-col-2-picture"></div>
+    <div class="row-5-col-1-picture"></div>
+    <div class="row-5-col-2-text">
+        <h2>
+            Pollution of the world's oceans.
+        </h2>
+        <p>
+            70% of our planet's surface is water. The ocean is a major factor in shaping Earth's climate:
+            ocean currents cause warming or cooling, and evaporated water forms clouds and causes precipitation.
+        </p>
+    </div>
+    <div class="row-6-text">
+        <h2>
+            System cleaning device
+        </h2>
+        <ul>
+            <li>Removal of organic residues, petroleum products</li>
+            <li>Destroying pathogens, creating no-drain technology, purifying brines from desalination</li>
+            <li>Removal of coarse particles, sand, grease</li>
+        </ul>
+    </div>
+    <div class="row-7-col-1">
+        <h2 id="row-7-col-1-text">Green Logistics</h2>
+    </div>
+    <div class="row-7-col-2">
+        <ul id="row-7-col-2-list">
+            <li>Carbon Offsetting</li>
+            <li>Clean Fuel & Technology</li>
+            <li>Carbon Reports</li>
+            <li>Green Optimization</li>
+        </ul>
+    </div>
+    <div class="row-8-col-1-picture"></div>
+    <div class="row-8-col-2-text">
+        <h3>
+            Physico-chemical cleaning
+        </h3>
+        <div id="row-8-col-2-tab-container">
+
+            <div class="row-8-col-2-tabs">
+                <h5>MOLE</h5>
+                <hr class="thin-line">
+                <hr class="fat-line">
+                <p>rakes, steps, drums</p>
+            </div>
+
+            <div class="row-8-col-2-tabs">
+                <h5>SUMPS</h5>
+                <hr class="thin-line">
+                <hr class="fat-line">
+                <p>radial,horizontal,lamellar</p>
+            </div>
+
+            <div class="row-8-col-2-tabs">
+                <h5>SUMPS</h5>
+                <hr class="thin-line">
+                <hr class="fat-line">
+                <p>radial,horizontal,lamellar</p>
+            </div>
+
+            <div class="row-8-col-2-tabs">
+                <h5>SUMPS</h5>
+                <hr class="thin-line">
+                <hr class="fat-line">
+                <p>radial,horizontal,lamellar</p>
+            </div>
+        </div>
+    </div>
+    <div class="row-9-footer">
+        <p id="footer-text-1">Email </p>
+        <p id="footer-text-2">info@elements-inc.eu</p>
+        <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>
+    </div>
+</div>
+
+
+<script src="js/scripts.js?<?php echo microtime(); ?>"></script>
+</body>
