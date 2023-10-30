5078b057990cc43ea773a3ab1842bb9646f626a9
Emils <emils.boreiko@gmail.com>
2023-10-30 23:46:45 +0200

    gg

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Elements Inc</title>
-    <link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles/styles.css?<?php echo microtime();?>">
     <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
 </head>
 <body>
@@ -145,98 +145,12 @@
     </div>
 </div>
 
-<div class="row-9-footer">
+<div class="row-9-footer" style="padding-bottom:35px;">
     <p id="footer-text-1">Email </p>
     <a id="footer-text-2" href="mailto:info@elements-inc.eu">info@elements-inc.eu</a>
-    <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>
+    <p id="footer-text-3">© <?php echo date("Y");?> by Elements-Inc. All rights reserved.</p>
 </div>
 
-<!--<div class="container">-->
-
-    <!--    <div class="row-4-col-1-text">-->
-    <!--        <h2 id="row-4-col-1-text-1">-->
-    <!--            Hazardous elements-->
-    <!--        </h2>-->
-    <!--        <p id="row-4-col-1-text-2">-->
-    <!--            <strong>Lead, chromium, cadmium, benzopyrene,</strong> and <strong>chlorine</strong> in the water cause cancer and nerve problems.-->
-    <!--        </p>-->
-    <!--        <p id="row-4-col-1-text-3">-->
-    <!--            Their effective and comprehensive removal is possible only with the use-->
-    <!--            of advanced wastewater treatment technologies and modern equipment.-->
-    <!--        </p>-->
-    <!--    </div>-->
-    <!--    <div class="row-4-col-2-picture"></div>-->
-
-    <!--    <div class="row-6-text">-->
-    <!--        <video autoplay loop muted>-->
-    <!--            <source src="/images/row-6-background.mp4" type="video/mp4">-->
-    <!--            Your browser does not support the video tag.-->
-    <!--        </video>-->
-    <!--        <h2>-->
-    <!--            System cleaning device-->
-    <!--        </h2>-->
-    <!--        <ul class="three-column-list">-->
-    <!--            <li>Removal of organic residues, petroleum products</li>-->
-    <!--            <li>Destroying pathogens, creating no-drain technology, purifying brines from desalination</li>-->
-    <!--            <li>Removal of coarse particles, sand, grease</li>-->
-    <!--        </ul>-->
-    <!--    </div>-->
-
-<!--    <div class="row-7-col-1">-->
-<!--        <h2 id="row-7-col-1-text">Green Logistics</h2>-->
-<!--    </div>-->
-<!--    <div class="row-7-col-2">-->
-<!--        <ul id="row-7-col-2-list">-->
-<!--            <li>Carbon Offsetting</li>-->
-<!--            <li>Clean Fuel & Technology</li>-->
-<!--            <li>Carbon Reports</li>-->
-<!--            <li>Green Optimization</li>-->
-<!--        </ul>-->
-<!--    </div>-->
-
-<!--    <div class="row-8-col-1-picture rellax" data-rellax-speed="-10"></div>-->
-<!--    <div class="row-8-col-2-text">-->
-<!--        <h3>-->
-<!--            Physico-chemical cleaning-->
-<!--        </h3>-->
-<!--        <div id="row-8-col-2-tab-container">-->
-<!---->
-<!--            <div class="row-8-col-2-tabs">-->
-<!--                <h5>MOLE</h5>-->
-<!--                <hr class="thin-line">-->
-<!--                <hr class="fat-line">-->
-<!--                <p>rakes, steps, drums</p>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="row-8-col-2-tabs">-->
-<!--                <h5>SUMPS</h5>-->
-<!--                <hr class="thin-line">-->
-<!--                <hr class="fat-line">-->
-<!--                <p>radial,horizontal,lamellar</p>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="row-8-col-2-tabs">-->
-<!--                <h5>SUMPS</h5>-->
-<!--                <hr class="thin-line">-->
-<!--                <hr class="fat-line">-->
-<!--                <p>radial,horizontal,lamellar</p>-->
-<!--            </div>-->
-<!---->
-<!--            <div class="row-8-col-2-tabs">-->
-<!--                <h5>SUMPS</h5>-->
-<!--                <hr class="thin-line">-->
-<!--                <hr class="fat-line">-->
-<!--                <p>radial,horizontal,lamellar</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-
-<!--    <div class="row-9-footer">-->
-<!--        <p id="footer-text-1">Email </p>-->
-<!--        <a id="footer-text-2" href="mailto:info@elements-inc.eu">info@elements-inc.eu</a>-->
-<!--        <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>-->
-<!--    </div>-->
-<!--</div>-->
 
 
 <script src="js/scripts.js?<?php echo microtime(); ?>"></script>
