4cd8538ecde574155788728c73dd3dac66ca1d54
Emils <emils.boreiko@gmail.com>
2023-10-30 05:16:22 +0200

    changes

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
 <body>
 
 
-<div class="container">
-    <div class="row-1-header rellax" data-rellax-speed="-10" id="row-1-header">
+<div class="row-1-header rellax" data-rellax-speed="-10" id="row-1-header">
+    <div class="row-1-header-text-container">
         <h1 class="rellax" data-rellax-speed="1">Elements Inc.</h1>
         <h2 class="rellax" data-rellax-speed="1">Only one Earth</h2>
     </div>
+</div>
 
+<div class="row_container_t1">
     <div class="row-2-col-1-text">
         <p id="row-2-col-1-text-1">
             The urgent problem of clean water is inextricably linked with the problem of good wastewater treatment.
@@ -30,31 +32,38 @@
         </p>
     </div>
     <div class="row-2-col-2-picture" id="row-2-col-2-slideshow"></div>
+</div>
 
-    <div class="row-3-text">
-        <video autoplay loop muted>
-            <source src="/images/row-3-background.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <h2>
-            The protection of surface waters from pollution is the most important task facing modern society.
-        </h2>
-    </div>
+<div class="row-3-text">
+    <video autoplay loop muted>
+        <source src="/images/row-3-background.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h2>
+        The protection of surface waters from pollution is the most important task facing modern society.
+    </h2>
+</div>
 
-    <div class="row-4-col-1-text">
-        <h2 id="row-4-col-1-text-1">
-            Hazardous elements
-        </h2>
-        <p id="row-4-col-1-text-2">
-            <strong>Lead, chromium, cadmium, benzopyrene,</strong> and <strong>chlorine</strong> in the water cause cancer and nerve problems.
-        </p>
-        <p id="row-4-col-1-text-3">
-            Their effective and comprehensive removal is possible only with the use
-            of advanced wastewater treatment technologies and modern equipment.
-        </p>
+<div class="row_container_t2" style="position:relative; background:white;">
+    <div class="row-4-col-1-text" style="background:white;">
+        <div style="width:80%; margin:auto;">
+            <h2 id="row-4-col-1-text-1">
+                Hazardous elements
+            </h2>
+            <p id="row-4-col-1-text-2">
+                <strong>Lead, chromium, cadmium, benzopyrene,</strong> and <strong>chlorine</strong> in the water cause
+                cancer and nerve problems.
+            </p>
+            <p id="row-4-col-1-text-3">
+                Their effective and comprehensive removal is possible only with the use
+                of advanced wastewater treatment technologies and modern equipment.
+            </p>
+        </div>
     </div>
     <div class="row-4-col-2-picture"></div>
+</div>
 
+<div class="row_container_t2">
     <div class="row-5-col-1-picture" id="row-5-col-1-slideshow"></div>
     <div class="row-5-col-2-text">
         <div class="row-5-col-2-text-background rellax" data-rellax-speed="-10"></div>
@@ -66,34 +75,38 @@
             ocean currents cause warming or cooling, and evaporated water forms clouds and causes precipitation.
         </p>
     </div>
+</div>
 
-    <div class="row-6-text">
-        <video autoplay loop muted>
-            <source src="/images/row-6-background.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-        <h2>
-            System cleaning device
-        </h2>
-        <ul class="three-column-list">
-            <li>Removal of organic residues, petroleum products</li>
-            <li>Destroying pathogens, creating no-drain technology, purifying brines from desalination</li>
-            <li>Removal of coarse particles, sand, grease</li>
-        </ul>
-    </div>
+<div class="row-6-text">
+    <video autoplay loop muted>
+        <source src="/images/row-6-background.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h2>
+        System cleaning device
+    </h2>
+    <ul class="three-column-list">
+        <li>Removal of organic residues, petroleum products</li>
+        <li>Destroying pathogens, creating no-drain technology, purifying brines from desalination</li>
+        <li>Removal of coarse particles, sand, grease</li>
+    </ul>
+</div>
 
-    <div class="row-7-col-1">
-        <h2 id="row-7-col-1-text">Green Logistics</h2>
-    </div>
-    <div class="row-7-col-2">
-        <ul id="row-7-col-2-list">
-            <li>Carbon Offsetting</li>
-            <li>Clean Fuel & Technology</li>
-            <li>Carbon Reports</li>
-            <li>Green Optimization</li>
-        </ul>
-    </div>
+<!--<div class="row_container_t2" style="height:50vh;">-->
+<!--    <div class="row-7-col-1">-->
+<!--        <h2 id="row-7-col-1-text">Green Logistics</h2>-->
+<!--    </div>-->
+<!--    <div class="row-7-col-2">-->
+<!--        <ul id="row-7-col-2-list">-->
+<!--            <li>Carbon Offsetting</li>-->
+<!--            <li>Clean Fuel & Technology</li>-->
+<!--            <li>Carbon Reports</li>-->
+<!--            <li>Green Optimization</li>-->
+<!--        </ul>-->
+<!--    </div>-->
+<!--</div>-->
 
+<div class="row_container_t2">
     <div class="row-8-col-1-picture rellax" data-rellax-speed="-10"></div>
     <div class="row-8-col-2-text">
         <h3>
@@ -130,13 +143,100 @@
             </div>
         </div>
     </div>
-
-    <div class="row-9-footer">
-        <p id="footer-text-1">Email </p>
-        <a id="footer-text-2" href="mailto:info@elements-inc.eu">info@elements-inc.eu</a>
-        <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>
-    </div>
 </div>
+
+<div class="row-9-footer">
+    <p id="footer-text-1">Email </p>
+    <a id="footer-text-2" href="mailto:info@elements-inc.eu">info@elements-inc.eu</a>
+    <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>
+</div>
+
+<!--<div class="container">-->
+
+    <!--    <div class="row-4-col-1-text">-->
+    <!--        <h2 id="row-4-col-1-text-1">-->
+    <!--            Hazardous elements-->
+    <!--        </h2>-->
+    <!--        <p id="row-4-col-1-text-2">-->
+    <!--            <strong>Lead, chromium, cadmium, benzopyrene,</strong> and <strong>chlorine</strong> in the water cause cancer and nerve problems.-->
+    <!--        </p>-->
+    <!--        <p id="row-4-col-1-text-3">-->
+    <!--            Their effective and comprehensive removal is possible only with the use-->
+    <!--            of advanced wastewater treatment technologies and modern equipment.-->
+    <!--        </p>-->
+    <!--    </div>-->
+    <!--    <div class="row-4-col-2-picture"></div>-->
+
+    <!--    <div class="row-6-text">-->
+    <!--        <video autoplay loop muted>-->
+    <!--            <source src="/images/row-6-background.mp4" type="video/mp4">-->
+    <!--            Your browser does not support the video tag.-->
+    <!--        </video>-->
+    <!--        <h2>-->
+    <!--            System cleaning device-->
+    <!--        </h2>-->
+    <!--        <ul class="three-column-list">-->
+    <!--            <li>Removal of organic residues, petroleum products</li>-->
+    <!--            <li>Destroying pathogens, creating no-drain technology, purifying brines from desalination</li>-->
+    <!--            <li>Removal of coarse particles, sand, grease</li>-->
+    <!--        </ul>-->
+    <!--    </div>-->
+
+<!--    <div class="row-7-col-1">-->
+<!--        <h2 id="row-7-col-1-text">Green Logistics</h2>-->
+<!--    </div>-->
+<!--    <div class="row-7-col-2">-->
+<!--        <ul id="row-7-col-2-list">-->
+<!--            <li>Carbon Offsetting</li>-->
+<!--            <li>Clean Fuel & Technology</li>-->
+<!--            <li>Carbon Reports</li>-->
+<!--            <li>Green Optimization</li>-->
+<!--        </ul>-->
+<!--    </div>-->
+
+<!--    <div class="row-8-col-1-picture rellax" data-rellax-speed="-10"></div>-->
+<!--    <div class="row-8-col-2-text">-->
+<!--        <h3>-->
+<!--            Physico-chemical cleaning-->
+<!--        </h3>-->
+<!--        <div id="row-8-col-2-tab-container">-->
+<!---->
+<!--            <div class="row-8-col-2-tabs">-->
+<!--                <h5>MOLE</h5>-->
+<!--                <hr class="thin-line">-->
+<!--                <hr class="fat-line">-->
+<!--                <p>rakes, steps, drums</p>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="row-8-col-2-tabs">-->
+<!--                <h5>SUMPS</h5>-->
+<!--                <hr class="thin-line">-->
+<!--                <hr class="fat-line">-->
+<!--                <p>radial,horizontal,lamellar</p>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="row-8-col-2-tabs">-->
+<!--                <h5>SUMPS</h5>-->
+<!--                <hr class="thin-line">-->
+<!--                <hr class="fat-line">-->
+<!--                <p>radial,horizontal,lamellar</p>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="row-8-col-2-tabs">-->
+<!--                <h5>SUMPS</h5>-->
+<!--                <hr class="thin-line">-->
+<!--                <hr class="fat-line">-->
+<!--                <p>radial,horizontal,lamellar</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!--    <div class="row-9-footer">-->
+<!--        <p id="footer-text-1">Email </p>-->
+<!--        <a id="footer-text-2" href="mailto:info@elements-inc.eu">info@elements-inc.eu</a>-->
+<!--        <p id="footer-text-3">© 2022 by Elements-Inc. All rights reserved.</p>-->
+<!--    </div>-->
+<!--</div>-->
 
 
 <script src="js/scripts.js?<?php echo microtime(); ?>"></script>
